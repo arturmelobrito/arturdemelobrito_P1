eb17a30d629d3c3390a93769398f7a182ebd127d
arturmelobrito <56053140+arturmelobrito@users.noreply.github.com>
2019-10-01 23:20:06 -0300

    Questão 3 antes de ser corrigida

--- Q3.py
+++ Q3.py
@@ -1,5 +1,3 @@
-#Pratica pra P1 de comp
-
 #Questão 3 da P1- Cálculo de pi
 def seq(n):
 	return (1/(2*n+1))
@@ -12,7 +10,8 @@ inversor=1
 while((p2-p1)>5*(10**-8) or (p2-p1)<-5*(10**-8) ):
 
 	p1=p2
-	p2=p2+4*seq(contador)*inversor
+	#Comentário do erro: coloquei um ponto ao invés de um asterisco para multiplicar -inversor-
+	p2=p2+4*seq(contador).inversor
 
 	contador+=1
 	inversor= -inversor
